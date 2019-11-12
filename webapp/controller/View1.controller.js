861d846abe1260490f9a6279083a48e969fec87e
suneelkumarreddy1991 <suneelkumarreddy.m@gmail.com>
2019-11-12 16:54:33 +0000

    Inserted a comment in view1 controller

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.hul.git.zdemoapp.ZDemoApp.controller.View1", {
+		onInit: function () {
+			//test1
+		}
+	});
+});
